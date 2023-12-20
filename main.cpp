b8a56bfd8631be0d8c70e13ebf2b24ff6b0bf9b5
Jelletje14 <101749486+Jelletje14@users.noreply.github.com>
2023-12-20 10:25:29 +0100

    update main

--- main.cpp
+++ main.cpp
@@ -20,61 +20,64 @@ ArmInformatie armInformatie;
 
 int main()
 {
-    // Initialise the digital pin LED1 as an output
-#ifdef LED1
-    DigitalOut led(LED1);
-#else
-    bool led;
-#endif
+    double schroefLengte = 15;
 
-    while (true) {
-        led = !led;
-        ThisThread::sleep_for(Timer_Check);
+    armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+    while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is 
+    armInformatie.set_Request_Status(armInformatie.Wacht); // zeg tegen de arm dat die moet wachten
+
+    while (screwEndEffectorInformatie.get_SchroefAanweezig()) // waneer er een schroef aanweezig is
+    {
+        armInformatie.set_Request_Status(armInformatie.Verwijder_schroef); // zeg tegen de arm dat die de schroef moet verwijderen
+        while (armInformatie.get_Actual_Status() != armInformatie.Verwijder_schroef); // wacht tot de arm klaar is met verijwderen van de schroef
+
+        armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+        while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is
     }
 
-    armInformatie.set_Request_Status(armInformatie.Meten);
-    while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie()));
-    armInformatie.set_Request_Status(armInformatie.Wacht);
-
-    if (screwEndEffectorInformatie.get_SchroefAanweezig()) 
+    while (screwEndEffectorInformatie.get_BitjeAanweezig()) // waneer er een bitje aanweezig is
     {
-        armInformatie.set_Request_Status(armInformatie.Verwijder_schroef);
-        while (armInformatie.get_Actual_Status() != armInformatie.Verwijder_schroef);
+        armInformatie.set_Request_Status(armInformatie.Verwijder_bitje); // zeg tegen de arm dat die de schroef moet verwijderen
+        while (!screwEndEffectorInformatie.drop_bitje());// wacht tot de arm klaar is met verijwderen van de schroef
 
-        armInformatie.set_Request_Status(armInformatie.Wacht);
-    }
-
-    if (screwEndEffectorInformatie.get_BitjeAanweezig()) 
-    {
-        armInformatie.set_Request_Status(armInformatie.Verwijder_bitje);
-        while (!screwEndEffectorInformatie.drop_bitje());
-
-        armInformatie.set_Request_Status(armInformatie.Wacht);
+        armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+        while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is
     }
 
     while (true) 
     {
-        while (!screwEndEffectorInformatie.get_BitjeAanweezig()) 
+        while (!screwEndEffectorInformatie.get_BitjeAanweezig()) // waneer er geen bitje aanwezig is
         {
-            armInformatie.set_Request_Status(armInformatie.Pak_bitje);
-            while (!screwEndEffectorInformatie.pick_bitje());
+            armInformatie.set_Request_Status(armInformatie.Pak_bitje); // zeg tegen de arm dat die een bitje moet gaan op pakken 
+            while (!screwEndEffectorInformatie.pick_bitje()); // wacht tot het bitje is opgepakt
 
-            armInformatie.set_Request_Status(armInformatie.Meten);
-            while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie()));
+            armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+            while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is
         }
 
-        while (screwEndEffectorInformatie.get_SchroefLengte() != screwEndEffectorInformatie.s) {
-        statements
+        while (!(   screwEndEffectorInformatie.get_SchroefLengte() < schroefLengte + 1 
+        &&          screwEndEffectorInformatie.get_SchroefLengte() > schroefLengte - 1)) // waneer de schroeflengte meer dan 1 mm afwijkt
+        {   
+            while (screwEndEffectorInformatie.get_SchroefAanweezig()) // waneer er een schroefaanwezig is
+            {
+                armInformatie.set_Request_Status(armInformatie.Verwijder_schroef);  // zeg tegen de arm dat die de schroef moet verwijderen
+                while (armInformatie.get_Actual_Status() != armInformatie.Verwijder_schroef); // wacht tot de arm klaar is met verijwderen van de schroef
+
+                armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+                while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is
+            }
+
+            while (!screwEndEffectorInformatie.get_SchroefAanweezig()) // waneer er geen schroef aanwezig is
+            {
+                armInformatie.set_Request_Status(armInformatie.Pak_schroef); // zeg tegen de arm dat die een schroef moet oppakken
+                while (!screwEndEffectorInformatie.pick_screw()); // wacht tot de schroef is op gepakt
+
+                armInformatie.set_Request_Status(armInformatie.Meten); // zeg tegen de arm dat die moet gaan meten
+                while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie())); // wacht tot de het meten voltooid is
+            }
         }
 
-        while (!screwEndEffectorInformatie.get_SchroefAanweezig()) 
-        {
-            armInformatie.set_Request_Status(armInformatie.Pak_bitje);
-            while (!screwEndEffectorInformatie.pick_screw());
-
-            armInformatie.set_Request_Status(armInformatie.Meten);
-            while (!screwEndEffectorInformatie.do_measurement(armInformatie.get_Huidige_Positie()));
-        }
+        
        
       
 
