814bcdafb7323d8eca007494c3edff9aed5251ea
ArendGr <151929798+ArendGr@users.noreply.github.com>
2024-01-06 14:49:18 +0100

    tekst bij arminformatie
    tekst bij arminformatie

--- ArmInformatie.cpp
+++ ArmInformatie.cpp
@@ -2,11 +2,14 @@
 #include <cmath>
 
 void ArmInformatie::set_Gemeten_Positie(Positie positie) {
+    // bij het meten van een nieuwe positie wordt eerst de vorig 'huidige' positie als oud neergezet
     oud = huidige;
     huidige = positie;
+    // Dit blok zou langer zijn mits er een daadwerkelijke robotarm is om mee te werken
 }
 
 void ArmInformatie::set_Start() {
+    // de huidige positie wordt als startpositie neergezet
     Start = huidige;
 }
 
@@ -18,21 +21,26 @@ double ArmInformatie::PositieVerschil(Positie huidige, Positie oud) {
 }
 
 Positie ArmInformatie::get_Start() {
+    // hier wordt de startpositie teruggegeven
     return Start;
 }
 
 Positie ArmInformatie::get_Huidige_Positie() {
+    // hier wordt de huidige positie teruggegeven
     return huidige;
 }
 
 Positie ArmInformatie::get_Vorige_Positie(Positie oud) {
+    // hier wordt de vorige positie teruggegeven
     return oud;
 }
 
 void ArmInformatie::set_Request_Status(Status status) {
+    // het setten van de opgevraagde status
     Request_Status = status;
 }
 
 ArmInformatie::Status ArmInformatie::get_Actual_Status() {
+    // hier wordt de actuele status teruggegeven
     return Actual_Status;
 }
--- ArmInformatie.h
+++ ArmInformatie.h
@@ -1,6 +1,6 @@
 #include "mbed.h"
 
-struct Positie {
+struct Positie { //Dit is de positie van het uiteinde van de robotarm waar de ScrewEndEffector op komt te zitten
     public:
     float x;
     float y;
@@ -10,7 +10,8 @@ struct Positie {
 class ArmInformatie { // Dit is de informatie die van de arm naar de ScrewEndEffector
 public:
 
-enum Status {
+enum Status { // Dit zijn de functies die de arm kan oproepen om uitgevoerd te worden
+              // In dit programma wordt een arm gesimuleerd omdat er geen fysieke arm is om te te gebruiken, dus de functies worden in de main aangeroepen
     Wacht,
     Pak_bitje,
     Pak_schroef,
@@ -22,27 +23,27 @@ enum Status {
 
 
 private:
-Positie huidige;
-Positie oud;
-Positie Start;
+Positie huidige; // Dit is de huidige positie van de armkop
+Positie oud;     // Dit is de vorige positie van de armkop
+Positie Start;   // Dit is de startpositie van de armkop
 
-Status Request_Status;
-Status Actual_Status;
+Status Request_Status; // Dit is de status die verzocht is
+Status Actual_Status; // Dit is de status die op het huidige moment gaande is
   
 
 public:
-    void set_Gemeten_Positie(Positie positie);
-    void set_Start();
+// setters
+    void set_Gemeten_Positie(Positie positie); //setter gemeten positie
+    void set_Start(); //setter start positie
+    void set_Request_Status(Status status); // setter request status
 
-    double PositieVerschil(Positie huidige, Positie oud);
+    double PositieVerschil(Positie huidige, Positie oud); // hier wordt het positieverschil berekend tussen de huidige positie en de vorige positie
 
-    Positie get_Huidige_Positie();
-    Positie get_Vorige_Positie(Positie oud);
-    Positie get_Start();
-
-    void set_Request_Status(Status status);
-
-    Status get_Actual_Status();
+// getters
+    Positie get_Huidige_Positie(); // getter huidige positie
+    Positie get_Vorige_Positie(Positie oud); // getter vorige positie
+    Positie get_Start(); // getter startpositie
+    Status get_Actual_Status(); // getter huidig actieve status
 
 
 };
