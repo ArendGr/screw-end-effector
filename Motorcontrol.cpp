8916b1326e144749e8d509de5e2832b4aeccd4e9
ArendGr <151929798+ArendGr@users.noreply.github.com>
2024-01-06 23:45:32 +0100

    bijna klaar
    bijna klaar, alleen nog een functie in ScrewEndEffectorInformatie.cpp en .h schrijven en nog het in de main voeren

--- Motorcontrol.cpp
+++ Motorcontrol.cpp
@@ -14,7 +14,7 @@ Positie positie;
 
 Timer statuscheck;
 
- /*
+    /*
     * SchroefAandrukStatus() van 0 betekend dat er geen druk is gedetecteerd in de sensoren
     * SchroefAandrukStatus() van 1 betekend dat er lichte druk is geidentificeerd
     * SchroefAandrukStatus() van 2 betekend dat er een ideale druk is voor het aanschroeven
@@ -26,7 +26,8 @@ bool Schroefmoment() {// Hier wordt bepaald naar welke staat van het aandraaien 
 
     // Meet de afstand tussen de huidige en vorige positie
     double schroeflengte;
-    double afstandsverschil = Arminformatie.PositieVerschil(Arminformatie.get_Huidige_Positie(), Arminformatie.get_Vorige_Positie(positie));
+    double afstandsverschil = Arminformatie.PositieVerschil(Arminformatie.get_Huidige_Positie(), Arminformatie.get_Vorige_Positie(positie)); 
+    // tijdens de uitvoering/demonstratie van het programma kan er niet gebruik gemaakt worden van een afstandsverschil aangezien er geen fysieke arm is om die informatie te leveren  
 
     bool goedbezig;
     switch(CEEinformatie.get_SchroefAandrukStatus()) {
@@ -71,18 +72,36 @@ bool Schroefmoment() {// Hier wordt bepaald naar welke staat van het aandraaien 
             goedbezig = false;
             }
             else if (status == ZachtDraaien) {
-            // de schroef is zachtjes ergens in aan het draaien en er is normale druk gedetecteerd. draai nu op normale snelheid    
-            status = AandraaiStatus::NormaalDraaien;    
-            goedbezig = true;    
+            if(afstandsverschil > (schroeflengte - 0.001)) { // <-- dit kan vervangen worden door een knop
+                // de schroef is erin!
+                status = AandraaiStatus::idle;
+                goedbezig = true;
+                } else {   
+                // de schroef is zachtjes ergens in aan het draaien en er is normale druk gedetecteerd. draai nu op normale snelheid    
+                status = AandraaiStatus::NormaalDraaien;    
+                goedbezig = true;   
+                } 
             }
             else if (status == NormaalDraaien) {
-            // de schroef is op normale snelheid met normale druk aan het schroeven, draai nu op harde snelheid
-            status = AandraaiStatus::HardDraaien;
-            goedbezig = true;
+                if(afstandsverschil > (schroeflengte - 0.001)) { // <-- dit kan vervangen worden door een knop
+                // de schroef is erin!
+                status = AandraaiStatus::idle;
+                goedbezig = true;
+                } else {    
+                // de schroef is op normale snelheid met normale druk aan het schroeven, draai nu op harde snelheid
+                status = AandraaiStatus::HardDraaien;
+                goedbezig = true;
+                }
             }
             else if (status == HardDraaien) {
-            // de schroef is nu hard aan het draaien met normale druk, ga zo door
-            goedbezig = true;    
+                if(afstandsverschil > (schroeflengte - 0.001)) { // <-- dit kan vervangen worden door een knop
+                // de schroef is erin!
+                status = AandraaiStatus::idle;
+                goedbezig = true;
+                } else {   
+                // de schroef is nu hard aan het draaien met normale druk, ga zo door
+                goedbezig = true;    
+                }
             }
         break;
          case 3:
@@ -91,7 +110,7 @@ bool Schroefmoment() {// Hier wordt bepaald naar welke staat van het aandraaien 
             goedbezig = false;
             }
             else if (status == ZachtDraaien) {
-                if(afstandsverschil > (schroeflengte - 0.001)) {
+                if(afstandsverschil > (schroeflengte - 0.001)) { // <-- dit kan vervangen worden door een knop
                     // de schroef is erin!
                     status = AandraaiStatus::idle;
                     goedbezig = true;
@@ -103,7 +122,7 @@ bool Schroefmoment() {// Hier wordt bepaald naar welke staat van het aandraaien 
 
             }
             else if (status == NormaalDraaien) {
-                if(afstandsverschil > (schroeflengte - 0.001)) {
+                if(afstandsverschil > (schroeflengte - 0.001)) { // <-- dit kan vervangen worden door een knop
                     // de schroef is erin!
                     status = AandraaiStatus::idle;
                     goedbezig = true;
@@ -125,22 +144,23 @@ bool Schroefmoment() {// Hier wordt bepaald naar welke staat van het aandraaien 
 }
 
 
-void DraaiSchroefAan() { // De boolean geeft aan of het uitvoeren van de functie gelukt is met true
+bool DraaiSchroefAan() { // De boolean geeft aan of het uitvoeren van de functie gelukt is met true
 
-    status = AandraaiStatus::ZachtDraaien; // Hier wordt de staat op zachtdraaien gezet (beginstand bij aanroepen van deze gehele functie)
-    bool SchroefIsErin = false; // Dit geeft aan dat de schroef er nog niet in is, omdat de functie net is aangeroepen
-    int Fouten = 0;
-    bool fout = false;
-    bool aangeraakt = false;
+    int staat = 0;
+    bool juist = false;
+    bool aangeraakt;
     double schroeflengte;
     Positie start; // start positie
     Positie huidige; // huidige positie
-    statuscheck.start(); // begin met tellen
-    double verschil = 0; 
-    bool error = false; // geen errors
 
-// Hieronder staat een while loop, dit blijft aan totdat het aandraaien van de schroef gelukt is of er een foutmelding gegeven wordt
-while ((verschil <= schroeflengte) && !error) {
+    if (staat == 0) {
+    status = AandraaiStatus::ZachtDraaien; // Hier wordt de staat op zachtdraaien gezet (beginstand bij aanroepen van deze gehele functie)
+    aangeraakt = false;
+    statuscheck.start(); // begin met tellen
+    staat = 1;
+    }
+    bool SchroefIsErin = false; // Dit geeft aan dat de schroef er nog niet in is, omdat de functie net is aangeroepen
+
     /*
     * Hieronder wordt de wisseling tussen de switch cases aangestuurd
     * Eerst wordt een bepaalde tijd gewacht totdat er geswitched kan worden, dit zorgt ervoor dat er geen onverwachte uitvoeringen gedaan worden door minescule overwachte imputs
@@ -156,59 +176,53 @@ while ((verschil <= schroeflengte) && !error) {
         aangeraakt = true;
     } else if (aangeraakt) {
         huidige = Arminformatie.get_Huidige_Positie();
-        verschil = abs(sqrt(pow(huidige.x - start.x, 2) + pow(huidige.y - start.y, 2) + pow(huidige.z - start.z, 2)));
+        double verschil = abs(sqrt(pow(huidige.x - start.x, 2) + pow(huidige.y - start.y, 2) + pow(huidige.z - start.z, 2)));
     } 
 
 
     if (statuscheck.read_ms() > 50) { // Wacht minstens 0.05s voordat er van case geswitched kan worden    
-        if (!Schroefmoment()) { 
-            fout = true;
-            Fouten++; // Er is +1 fout opgetreden
+        if (!Schroefmoment()) { // hierbij wordt de functie uitgevoerd en tegelijkertijd gekeken of alles wel in orde is
+            juist = false; // Er is +1 fout opgetreden
         }
         else { 
-            fout = false; 
-            if (Fouten > 0) {Fouten = 0;} // als er geen fout is en de vorige keer was er wel een fout, begint de teller weer bij 0
+            juist = true; // er is geen fout gemaakt
         }
 
-        statuscheck.reset();
+        statuscheck.reset(); // de timer reset zodat iedere 50 milliseconden gekeken kan worden naar een update
     }
-
-    if ((Fouten > 2) && fout) { // Er zijn 3 foute pogingen achter elkaar gemaakt, Er is iets mis, nu gaat het programma uit de functie, hier moet later een error functie voor komen
-        error = true;
-        return;
-        break;
-    }
-
 
     switch(status) { //Deze switchcase kan de status switchen tussen idle (niet draaien), Zachtdraaien, Normaaldraaien en Harddraaien
 
         case idle: // in deze staat gebeurd er niks en staat de motor uit
-        CEEinformatie.set_RPMmotor(0); // dit is 0% rpm speed
-        // Functie: Duw de schroef met 0 m/s aan
-        if (SchroefIsErin == true) { // Als de schroef erin is, dan wordt deze boolean functie teruggeven met true, hierboven wordt de RPM al op 0 gezet
+        if (SchroefIsErin == true) { 
+            CEEinformatie.set_RPMmotor(0); // dit is 0% rpm speed
+            // Als de schroef erin is, dan wordt deze boolean functie teruggeven met true, hierboven wordt de RPM al op 0 gezet
             // Functie: laat schroef los
-            return;
+            return true;
         }
 
         break;
         case ZachtDraaien: // in deze staat draait de motor op gedefinieerde "langzame" snelheid
-            CEEinformatie.set_RPMmotor(20); // dit is 20% rpm speed
+            CEEinformatie.set_RPMmotor(30); // dit is 20% rpm speed
             // Functie: Duw de schroef met 0.1 m/s aan
 
         break;    
         case NormaalDraaien: // in deze staat draait de motor op gedefinieerde "normale" snelheid
-            CEEinformatie.set_RPMmotor(50); // dit is 50% rpm speed
+            CEEinformatie.set_RPMmotor(60); // dit is 50% rpm speed
             // Functie: Duw de schroef met 0.1 m/s aan
 
         break;
         case HardDraaien: // in deze staat draait de motor op gedefinieerde "snelle" snelheid
-            CEEinformatie.set_RPMmotor(80); // dit is 80% rpm speed
+            CEEinformatie.set_RPMmotor(90); // dit is 80% rpm speed
             // Functie: Duw de schroef met 0.1 m/s aan
 
         break;
 
         }
-    }
-    status = AandraaiStatus::idle; // Hier wordt de staat op idle gezet (eindstand).. Als de taak klaar is of er is een error mag er niet meer aangedraaid worden
+
+        return juist;
 }; 
+
+// het programma is bijna af, alleen moet er nog een functie komen in ScrewEndEffectorInformatie.cpp en .h
+// en ik moet het gedeelte in de main schrijven zodat dit daadwerkelijk aangeroepen wordt
 
